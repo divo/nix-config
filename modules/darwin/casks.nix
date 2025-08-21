5e3c27aa7ad75ed47819f3a8e8768a623902304f
Steven <steven@nory.ai>
2025-08-21 09:31:14 +0100

    Appears to be working, start adding packages

--- modules/darwin/casks.nix
+++ modules/darwin/casks.nix
@@ -4,23 +4,28 @@ _:
   # Development Tools
   "homebrew/cask/docker"
   "visual-studio-code"
+  "zed"
+  "figma"
 
   # Productivity Tools
   "raycast"
+  "amethyst"
 
   # Browsers
   "google-chrome"
 
   # Communication Tools - Examples (uncomment as needed)
   # "discord"
-  # "notion"
-  # "slack"
+  "notion"
+  "slack"
   # "telegram"
-  # "zoom"
+  "zoom"
 
   # Utility Tools - Examples (uncomment as needed)
   # "syncthing"
-  # "1password"
+  "1password"
+  "warp"
+  "anki"
   # "rectangle"
 
   # Entertainment Tools - Examples (uncomment as needed)
